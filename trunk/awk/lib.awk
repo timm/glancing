3e5ef274083a54699ecb60d6bcecc3a71c7f2926
menzies.tim <menzies.tim@fab9183a-8bf2-11dd-8a42-c3ad82d480ce>
2008-09-26 18:02:06 +0000

    starting to glance

---
+++ trunk/awk/lib.awk
@@ -0,0 +1,57 @@
+#### random number seeds;
+BEGIN { srand(Seed ? Seed : seed()) }
+function seed(    r) {
+	com="bash -c 'echo $RANDOM'" 
+	com | getline r
+	close(com)
+	return r
+}
+#### convert a number into a string, suitable for sorting
+function sortable(n,   l,r,t) { 
+	l = l?l:40   # default lhs space
+	r = r?r:20   # default rhs space
+	t = t?t:-8   # default size of "tiny" numbers
+	return sprintf("%"l"."r"f",n+rand()/10^t) 
+}
+#### do nothing!
+function noop() { return 1 }
+
+#### stacks
+function pop(a)    { return a[a[0]--] }	
+function push(a,x) { a[++a[0]] = x    }	
+
+function pop2(a) { return a[a[0]--] }
+function push2(a,key,val,   i) {
+	i        = ++a[0]
+	a[i]    = key
+	a[-1*i] = val
+}	
+function _stack(  n,a) {
+	push(a,"mon"); push(a,"tue"); push(a,"wed");
+	push(a,"thu"); push(a,"fri");
+	saya("pushed",a)
+	n=a[0]
+	while(n--) print "after popping [" pop(a)"] a[0]="a[0]
+}
+#### print arrays, sorted on key
+function saya(s,a,    i,j,com) {
+	com= "sort # " rand()
+	for(i in a) {	
+		j=i 
+		gsub(SUBSEP,",",j);
+		print s "[" j "] = " a[i] | com;
+	}
+	close(com)
+}
+function _saya(  a) { a[1]=2;a[3]=4; saya("ss",a) }
+
+#### simple array initialization
+function s2a(s,a,  n,tmp,i) {
+	n=split(s,tmp,",");
+	for(i=1;i<=n; i += 2) 
+		a[tmp[i]]=tmp[i+1]
+}
+function _s2a(  a) {
+	s2a("mon,2,tue,4,wed,8,thu,1,fri,1",a);
+	saya("a",a)
+}
---
+++ trunk/mk/lib.mk
@@ -0,0 +1,60 @@
+#### standard stuff
+prefix= $(HOME)/opt/
+G=      $(prefix)glance
+D=      $$
+Tmp=    $G/tmp
+Want=   $G/var/want
+Dirs=   $G $(Tmp) $G/var $(Want) 
+Dirp=   if [ ! -d $d ]; then  mkdir $d; fi;
+This=   nothing
+
+#### stuff to define awk usage
+Src=     -f $(subst awk , awk -f ,$(Lib)) \
+		 -f $(subst awk , awk -f ,$(App)) \
+		 --source 
+Run=     gawk $(Src)
+Debug=   pgawk --dump-variables=$(Tmp)/vars.out $(Src)
+Profile= pgawk --profile=$(Tmp)/profile.out     $(Src)
+
+#### run all tests
+all  :;  @$(foreach x, $(shell $(MAKE) rules),$(MAKE) $x;)
+rules:;  @ gawk -F\: '/^[a-z].*:/ {print $D1}' Makefile 
+tests: dirs
+	@ $(MAKE) testEngine | tee $(Tmp)/tests.out
+	@ echo ""; cut -d\  -f 1 $(Tmp)/tests.out | sort | uniq -c 
+
+#### install stuff
+install: ready
+	@ cp etc/want $(Want)
+
+#### set up stuff
+ready: dirs files
+
+dirs  :; @ $(foreach d, $(Dirs), $(Dirp))
+files :  $(Lib) $(App)
+	@ touch $(Tmp)/profile.awk
+
+#### generic stuff
+# print all vars
+globals: ready
+	@ $(Debug) 'BEGIN { noop() }'
+	@ egrep -v "^[A-Z]*:" $(Tmp)/vars.out
+
+# run "What" and profile the output
+profile: ready 
+	$(MAKE) Run="$(Profile)" $(What)
+	@ cat $(Tmp)/profile.out 	
+
+nothing :; @ echo "usage: make [profile|test] What=something"
+
+#### unit tests
+testEngine:   
+	@$(foreach x, $(shell $(MAKE) rules),$(MAKE) test What=$x;) 
+
+test : $(Want)/$(What)
+	@ $(MAKE) $(What) > $(Tmp)/$(What)
+	@ if   diff -s $(Tmp)/$(What) $< > /dev/null ; then \
+           echo PASSED $(What) ; \
+      else echo FAILED $(What), see got $(Tmp)/$(What); fi\
+
+want :; @ $(MAKE) $(What) | tee $(Want)/$(What)
